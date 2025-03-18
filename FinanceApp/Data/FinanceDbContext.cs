00b5404d79cc358bdafc8338d2babdcce23e5c2a
Barth Z2 <barthgustavo5@gmail.com>
2025-03-18 14:21:50 -0300

    Add project files.

---
+++ FinanceApp/Data/FinanceDbContext.cs
@@ -0,0 +1,12 @@
+﻿using FinanceApp.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FinanceApp.API.Data
+{
+    public class FinanceDbContext : DbContext
+    {
+        public FinanceDbContext(DbContextOptions<FinanceDbContext> options) : base(options) { }
+
+        public DbSet<Transaction> Transactions { get; set; }
+    }
+}
---
+++ FinanceApp/Models/Transaction.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace FinanceApp.API.Models
+{
+    public class Transaction
+    {
+        public int Id { get; set; }
+        public decimal Amount { get; set; }
+        public string Category { get; set; }
+        public string Description { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
