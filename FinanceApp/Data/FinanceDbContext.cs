7c87b5153724644f2e75e06666099028b80ca5a2
Barth Z2 <barthgustavo5@gmail.com>
2025-03-27 16:59:58 -0300

    Configurando balance

--- FinanceApp/Data/FinanceDbContext.cs
+++ FinanceApp/Data/FinanceDbContext.cs
@@ -11,6 +11,7 @@ namespace FinanceApp.API.Data
 
         public DbSet<Transaction> Transactions { get; set; }
         public DbSet<User> Users { get; set; } // Se você tiver um modelo User
+        public DbSet<Balance> Balances { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
@@ -22,6 +23,13 @@ namespace FinanceApp.API.Data
                 .WithMany() // Assume que o User pode ter várias transações
                 .HasForeignKey(t => t.UserId)
                 .OnDelete(DeleteBehavior.Restrict); // Pode ser CASCADE ou outros comportamentos
+
+            // Configuração da relação entre Balance e User
+            modelBuilder.Entity<Balance>()
+                .HasOne(b => b.User)
+                .WithOne() // Um usuário tem um único saldo
+                .HasForeignKey<Balance>(b => b.UserId)
+                .OnDelete(DeleteBehavior.Cascade); // Se o usuário for deletado, o saldo também será
         }
     }
 }
---
+++ FinanceApp/Models/Balance.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using FinanceApp.Models;
+
+namespace FinanceApp.Models
+{
+    public class Balance
+    {
+        public int Id { get; set; }
+        public decimal Amount { get; set; } // O valor atual do saldo
+    }
+
+}
